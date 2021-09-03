109ac5c6868ef76497d68c29da15b864a526a06c
shortnickname <selimasha@mail.ru>
2021-09-03 16:58:07 +0700

    ховеры начало

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,5 +1,5 @@
 const burgerButton = document.querySelector('.main-navigation__toggle');
-const mainNav = document.querySelector('.main-navigation');
+const mainNav = document.querySelector('.page-header__main-navigation');
 
 burgerButton.addEventListener('click', function() {
   mainNav.classList.toggle('main-navigation--opened');
