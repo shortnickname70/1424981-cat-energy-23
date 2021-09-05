b2a49299c5727e3b48e05268393d3103827d3b5b
shortnickname70 <selimasha@mail.ru>
2021-09-05 18:39:04 +0700

    с

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,6 +1,10 @@
 const burgerButton = document.querySelector('.main-navigation__toggle');
 const mainNav = document.querySelector('.page-header__main-navigation');
+const checkbox = document.querySelector('.navigation__toggle-checkbox');
 
 burgerButton.addEventListener('click', function() {
   mainNav.classList.toggle('main-navigation--opened');
 })
+
+// checkbox.classList.add('navigation__toggle-checkbox--hidden');
+// burgerButton.classList.add('main-navigation__toggle--show');
