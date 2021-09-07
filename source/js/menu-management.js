cb9123bc1b254e1b6e1229789487d12243a0e9ba
shortnickname <selimasha@mail.ru>
2021-09-07 17:39:34 +0700

    меню на главной работает при выключенном js и включенном js

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,7 +1,10 @@
 const burgerButton = document.querySelector('.main-navigation__toggle');
 const mainNav = document.querySelector('.page-header__main-navigation');
 
-mainNav.classList.remove('.page-header__main-navigation');
+// mainNav.classList.remove('.page-header__main-navigation');
+
+// document.querySelector('.page-header__main-navigation').classList.remove('.page-header__main-navigation');
+mainNav.classList.add('main-navigation--closed');
 
 burgerButton.onclick = function()
 {mainNav.classList.toggle("main-navigation--closed");
