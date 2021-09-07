ad194da5c5ee43eaea41948a5859b0755c27574d
shortnickname <selimasha@mail.ru>
2021-09-07 16:56:09 +0700

    меню на главной без js

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,10 +1,13 @@
 const burgerButton = document.querySelector('.main-navigation__toggle');
 const mainNav = document.querySelector('.page-header__main-navigation');
-const checkbox = document.querySelector('.navigation__toggle-checkbox');
 
-burgerButton.addEventListener('click', function() {
-  mainNav.classList.toggle('main-navigation--opened');
-})
+mainNav.classList.remove('.page-header__main-navigation');
+
+burgerButton.onclick = function()
+{mainNav.classList.toggle("main-navigation--closed");
+mainNav.classList.toggle("main-navigation--opened");
+
+};
 
 // checkbox.classList.add('navigation__toggle-checkbox--hidden');
 // burgerButton.classList.add('main-navigation__toggle--show');
