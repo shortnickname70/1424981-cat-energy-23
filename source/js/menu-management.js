4b4e625bf6d088fe56c6edf6f9c0cfed2395f98f
shortnickname70 <selimasha@mail.ru>
2021-09-19 19:09:33 +0700

    фиксим ошибки еще

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,14 +1,14 @@
-const burgerButton = document.querySelector('.main-nav__toggle');
-const mainNav = document.querySelector('.main-nav');
+const burgerButton = document.querySelector(".main-nav__toggle");
+const mainNav = document.querySelector(".main-nav");
 
 // mainNav.classList.remove('.page-header__main-navigation');
 
 // document.querySelector('.page-header__main-navigation').classList.remove('.page-header__main-navigation');
-mainNav.classList.add("main-navigation--closed");
+mainNav.classList.add("main-nav--closed");
 
 burgerButton.onclick = function()
-{mainNav.classList.toggle("main-navigation--closed");
-mainNav.classList.toggle("main-navigation--opened");
+{mainNav.classList.toggle("main-nav--closed");
+mainNav.classList.toggle("main-nav--opened");
 
 };
 
