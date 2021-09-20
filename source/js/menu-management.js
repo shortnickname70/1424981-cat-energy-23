457658113d16325f947b77a02d4e69d95705063c
shortnickname <selimasha@mail.ru>
2021-09-20 14:40:17 +0700

    окончательные правки

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,9 +1,6 @@
 const burgerButton = document.querySelector(".main-nav__toggle");
 const mainNav = document.querySelector(".main-nav");
 
-// mainNav.classList.remove('.page-header__main-navigation');
-
-// document.querySelector('.page-header__main-navigation').classList.remove('.page-header__main-navigation');
 mainNav.classList.add("main-nav--closed");
 
 burgerButton.onclick = function()
@@ -12,5 +9,4 @@ mainNav.classList.toggle("main-nav--opened");
 
 };
 
-// checkbox.classList.add('navigation__toggle-checkbox--hidden');
-// burgerButton.classList.add('main-navigation__toggle--show');
+
